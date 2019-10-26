7099e9e5b2e57d9c7249569bb622f544e8cdaf50
buharov-alexander <buharovsasha@yandex.ru>
2019-10-26 10:11:05 +0700

    add comment

--- spring/IoC_Container/bean_post_processors/src/main/java/ru/bukharov/training/spring/Starter.java
+++ spring/IoC_Container/bean_post_processors/src/main/java/ru/bukharov/training/spring/Starter.java
@@ -5,5 +5,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Starter {
     public static void main(String[] args) {
         new AnnotationConfigApplicationContext(AppConfig.class);
+        /*
+            1) LoggerBeanFactoryPostProcessor get all bean definitions
+
+            2) Create bean "myHead"
+                2.1) Constructor myHead
+                2.2) LoggerBeanPostProcessor - before initialization myHead
+                2.3) Post construct for myHead
+                2.4) LoggerBeanPostProcessor - after initialization myHead
+
+            3) Create bean "myPerson"
+                3.1) Constructor myPerson
+                3.2) LoggerBeanPostProcessor - before initialization myPerson
+                3.3) Post construct for myPerson
+                3.4) LoggerBeanPostProcessor - after initialization myPerson
+
+            4) LoggerApplicationListener get ContextRefreshedEvent
+         */
     }
 }
